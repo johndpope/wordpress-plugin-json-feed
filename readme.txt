8315044df254ec8c610a114c909a5ca23649a13b
Claude Betancourt <cbetancourt@gmail.com>
2013-09-29 20:59:35 -0400

    Initial commit. Sample usage will be next.

--- readme.txt
+++ readme.txt
@@ -1,5 +1,11 @@
-wordpress-plugin-json-feed
-==========================
+=== SDJI JSON feed ===
+Contributors: chrisnorthwood, cbetancourt
+Tags: json, feed, feeds
+Requires at least: 3.6.1
+Tested up to: 3.6.1
+Stable tag: 1.0
+
+== Description ==
 
 This is a 'fork' of Chris Northwood's popular [JSON Feed plugin for version 1.3](http://wordpress.org/support/plugin/json-feed).
 
@@ -11,3 +17,11 @@ Below is a list of the changes made to version 1.3:
 * JSONP support has been modified to expect a parameter named `callback` instead of the original `jsonp`
 * The list of columns returned has been limited to 'title,' 'excerpt,' 'permalink,' 'display_date' and 'unix_date.'
 * 'title' and 'excerpt' are now stripped off tags and HTML entities encoded.
+
+== Frequently Asked Questions ==
+
+== Changelog ==
+
+= 1.0 =
+
+Initial version of this 'fork'
---
+++ wordpress-plugin-json-feed.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+  Plugin Name: JSON Feed Plugin for Wordpress
+  Description: This is a 'fork' of Chris Northwood's popular JSON Feed plugin for version 1.3 http://wordpress.org/support/plugin/json-feed. It provides he ability to generate feeds in JSON format from any place on your WordPress site.
+  Version: 1.0
+  Author: Modified by Claude Betancourt. Original by Chris Northwood and Dan Phiffer.
+ */
+
+	const DEFAULT_MAX_STORIES = 10;
+	const DEFAULT_DATE_FORMAT = 'F j, Y G:i e';
+
+add_filter('query_vars', 'json_feed_queryvars');
+
+function json_feed_queryvars($qvars) {
+    $qvars[] = 'callback'; //JSONP support
+    $qvars[] = 'df'; // date format
+    $qvars[] = 'limit';
+    return $qvars;
+}
+
+function json_feed() {
+    // set max items to display
+    $_items = trim(get_query_var('limit'));
+    $items = ($_items != '' && is_numeric($_items) && $_items > 0) ? trim(get_query_var('limit')) : DEFAULT_MAX_STORIES;
+
+    $output = array();
+    $count = 1;
+    while (have_posts() && $count <= $items) {
+	the_post();
+	$post_date = strtotime(get_the_date(DATE_W3C));
+	$output[] = array(
+	    'title' => htmlentities(strip_tags(get_the_title())),
+	    'excerpt' => htmlentities(strip_tags(get_the_excerpt())),
+	    'permalink' => get_permalink(),
+	    'display_date' => date(get_date_format(), $post_date),
+	    'unix_date' => $post_date * 1000
+	);
+	$count++;
+    }
+
+    if (!get_query_var('callback') == '') {
+	header('Content-Type: application/javascript; charset=' . get_option('blog_charset'), true);
+	echo get_query_var('callback') . '(' . json_encode($output) . ')';
+    } else {
+	header('Content-Type: application/json; charset=' . get_option('blog_charset'), true);
+	echo json_encode($output);
+    }
+}
+
+/*
+ * Retrieves the default date constant or uses the one passed via query param.
+ *
+ * Optionally, you can retrieve the date format configured for the blog,
+ * get_option('date_format')
+ *
+ */
+
+function get_date_format() {
+    if (get_query_var('df')) {
+	return get_query_var('df');
+    } else {
+	return DEFAULT_DATE_FORMAT;
+    }
+}
+
+add_action('do_feed_json', 'json_feed');
